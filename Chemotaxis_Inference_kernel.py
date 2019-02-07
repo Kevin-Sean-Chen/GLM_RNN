b02893c706ae27191fdd7cb1599ad1ea3dda2530
kschen <kschen@princeton.edu>
2019-02-07 16:56:52 -0500

    tuning kernel

--- Chemotaxis_Inference_kernel.py
+++ Chemotaxis_Inference_kernel.py
@@ -15,16 +15,36 @@ import scipy.optimize   #for log-likelihood
 from scipy.special import iv  #for Bessel function
 from scipy.stats import vonmises  #for von Mises distribution
 
+
+#incoporate the temporal kernel for chemo-sensing
+def temporal_kernel(alpha,tau):
+    """
+    biphasic temporal kernel
+    tau is the temporal domain and alpha controls the form
+    """
+    D_tau = alpha*np.exp(-alpha*tau)*((alpha*tau)**5/math.factorial(5) - (alpha*tau)**7/math.factorial(7))
+    return D_tau
+tt = np.linspace(0,10,10/0.6)
+plt.plot(tt,-temporal_kernel(2.,tt),'-o')
+
+#check with auto-correlation in time series
+def autocorr(x):
+    """
+    Autocorrelation function for time-series
+    """
+    result = np.correlate(x, x, mode='full')
+    return result[result.size // 2:]
+
 #Hybrid Gaussian process for angle time series
-def d_theta(alpha, dc_perp, K, w, dC):
+def d_theta(K_dcp, dc_perp, K, K_dc, dC):
     '''
     Return change in theta angle for each step
-    Input with alpha for weighting, dc for orthogonal concentration difference, and K covariance in weathervaning
-    W as the weighting/kernel on concentration in the signoidal function for tumbling rate 
+    Input with K_dcp for kernel, dc for orthogonal concentration difference, and K covariance in weathervaning
+    K_dc as the weighting/kernel on concentration in the signoidal function for tumbling rate 
     '''
-    wv = alpha*dc_perp + K*np.random.randn()  #weathervaning strategy
+    wv = np.dot(K_dcp,dc_perp) + K*np.random.randn()  #weathervaning strategy
     #P_event = 0.023/(0.4 + np.exp(40*dC/dt)) + 0.003  #sigmoidal function with parameters w
-    P_event = 5*0.023/(1 + np.exp(40*dC/dt))  #less parameter version
+    P_event = 5*0.023/(1 + np.exp(np.dot(K_dc,dC/dt)))  #less parameter version
     if np.random.rand() < P_event:
         beta = 1
     else:
@@ -40,11 +60,17 @@ def d_theta(alpha, dc_perp, K, w, dC):
 
 #concentration gradient in space
 def gradient(C0,x,y):
+    """
+    Gaussian sptatial profile through diffision equation
+    """
     concentration = C0/(4*np.pi*d*D*duT)*np.exp(-(x-dis2targ)**2/(400*D*duT*50))  #depends on diffusion conditions
     return concentration
 
 #measure for concentration difference for weathervane
 def dc_measure(dxy,xx,yy):
+    """
+    perpendicular concentration measure
+    """
     perp_dir = np.array([-dxy[1], dxy[0]])
     perp_dir = perp_dir/np.linalg.norm(perp_dir)
     perp_dC = gradient(C0, xx+perp_dir[0], yy+perp_dir[1]) - gradient(C0, xx-perp_dir[0], yy-perp_dir[1])
@@ -58,7 +84,11 @@ duT = 60*60*1
 d = 0.18
 
 #chemotaxis strategy parameter
-alpha = 20  #strength of OU forcing
+K_win = np.linspace(0,10,10/0.6)
+#K_dc = 3*(temporal_kernel(2.,K_win))+.0  #random-turning kernel (biphasic form)
+K_dc = -np.exp(-K_win/0.5) 
+wv_win = 0.1
+K_dcp = np.exp(-K_win/wv_win)  #weathervaning kernel (exponential form)
 K = 5  #covariance of weathervane
 w = 0  #logistic parameter (default for now)
 T = 3000
@@ -68,43 +98,46 @@ v_s = 0.01  #std of speed
 time = np.arange(0,T*dt,dt)
 xs = np.zeros(time.shape)
 ys = np.zeros(time.shape)  #2D location
-xs[0] = np.random.randn()
-ys[0] = np.random.randn()
+prehist = max(len(K_dc),len(K_dcp))  #pre-histroy length
+xs[:prehist] = np.random.randn(prehist)
+ys[:prehist] = np.random.randn(prehist)
 ths = np.zeros(time.shape)  #agle with 1,0
-ths[0] = np.random.randn()
+ths[:prehist] = np.random.randn(prehist)
 dxy = np.random.randn(2)
 
-### with turning (Brownian-like tragectories)
-#for t in range(1,len(time)):
-#    
-#    #concentration = gradient(C0,xs[t-1],ys[t-1])
-#    dC = gradient(C0, xs[t-1],ys[t-1]) - gradient(C0, xs[t-2],ys[t-2])
-#    dc_perp = dc_measure(dxy,xs[t-1],ys[t-1])      
-#    dth = d_theta(alpha, -dc_perp, K, 0, dC)
-#    ths[t] = ths[t-1] + dth*dt
-#    
-#    e1 = np.array([1,0])
-#    vec = np.array([xs[t-1],ys[t-1]])
-#    theta = math.acos(np.clip(np.dot(vec,e1)/np.linalg.norm(vec)/np.linalg.norm(e1), -1, 1)) #current orienation relative to (1,0)
-#
-#    vv = v_m + v_s*np.random.randn()
-#    dd = np.array([vv*np.sin(ths[t]*np.pi/180), vv*np.cos(ths[t]*np.pi/180)])  #displacement
-#    c, s = np.cos(theta), np.sin(theta)
-#    R = np.array(((c,s), (-s, c)))  #rotation matrix, changing coordinates
-#    dxy = np.dot(R,dd)
-#    
-#    xs[t] = xs[t-1] + dxy[0]*dt
-#    ys[t] = ys[t-1] + dxy[1]*dt
-#
-##plt.plot(ths)
-#plt.figure()
-#plt.plot(xs,ys)
-#plt.figure()
-#x = np.arange(np.min(xs),np.max(xs),1)
-#xx_grad = C0/(4*np.pi*d*D*duT)*np.exp(-(x-dis2targ)**2/(400*D*duT*50))
-#plt.imshow(np.expand_dims(xx_grad,axis=1).T,extent=[np.min(xs),np.max(xs),np.min(ys),np.max(ys)])
-#plt.hold(True)
-#plt.plot(xs,ys,'white')
+## with turning (Brownian-like tragectories)
+for t in range(prehist,len(time)):
+    
+    #concentration = gradient(C0,xs[t-1],ys[t-1])
+    #dC = gradient(C0, xs[t-1],ys[t-1]) - gradient(C0, xs[t-2],ys[t-2])
+    dC = np.array([gradient(C0, xs[t-past],ys[t-past]) for past in range(0,len(K_dc))])
+    #dc_perp = dc_measure(dxy,xs[t-1],ys[t-1])  
+    dc_perp = np.array([dc_measure(dxy, xs[t-past],ys[t-past]) for past in range(0,len(K_dcp))])    
+    dth = d_theta(K_dcp, -dc_perp, K, K_dc, dC)
+    ths[t] = ths[t-1] + dth*dt
+    
+    e1 = np.array([1,0])
+    vec = np.array([xs[t-1],ys[t-1]])
+    theta = math.acos(np.clip(np.dot(vec,e1)/np.linalg.norm(vec)/np.linalg.norm(e1), -1, 1)) #current orienation relative to (1,0)
+
+    vv = v_m + v_s*np.random.randn()
+    dd = np.array([vv*np.sin(ths[t]*np.pi/180), vv*np.cos(ths[t]*np.pi/180)])  #displacement
+    c, s = np.cos(theta), np.sin(theta)
+    R = np.array(((c,s), (-s, c)))  #rotation matrix, changing coordinates
+    dxy = np.dot(R,dd)
+    
+    xs[t] = xs[t-1] + dxy[0]*dt
+    ys[t] = ys[t-1] + dxy[1]*dt
+
+#plt.plot(ths)
+plt.figure()
+plt.plot(xs,ys)
+plt.figure()
+x = np.arange(np.min(xs),np.max(xs),1)
+xx_grad = C0/(4*np.pi*d*D*duT)*np.exp(-(x-dis2targ)**2/(400*D*duT*50))
+plt.imshow(np.expand_dims(xx_grad,axis=1).T,extent=[np.min(xs),np.max(xs),np.min(ys),np.max(ys)])
+plt.hold(True)
+plt.plot(xs,ys,'white')
 
 #####
 #Generate trajeectories
